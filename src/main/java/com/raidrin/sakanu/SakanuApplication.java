6fd9a4c0f2de7e3b530b89178328d50ebca62866
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2024-01-13 22:04:32 -0600

    Initial commit

---
+++ src/main/java/com/raidrin/sakanu/SakanuApplication.java
@@ -0,0 +1,13 @@
+package com.raidrin.sakanu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SakanuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SakanuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/raidrin/sakanu/SakanuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raidrin.sakanu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SakanuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
