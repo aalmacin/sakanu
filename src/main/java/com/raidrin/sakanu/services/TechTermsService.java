958c67864c6505750134a983372516d4efd65ff1
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2024-03-23 20:53:27 -0600

    Remove cache

--- src/main/java/com/raidrin/sakanu/services/TechTermsService.java
+++ src/main/java/com/raidrin/sakanu/services/TechTermsService.java
@@ -1,8 +1,6 @@
 package com.raidrin.sakanu.services;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.cache.interceptor.SimpleKey;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,7 +9,6 @@ public class TechTermsService {
     private final OpenAiTermQuery openAiTermQuery;
 
 
-    @Cacheable(value = "term")
     public TermResponse getTechTerm(String domain, String term) {
         return openAiTermQuery.query(domain, term);
     }
