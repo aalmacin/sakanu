b09ade16b21964d649b715550c9abdd1589eb644
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2024-05-06 17:55:08 -0600

    Add h2 for test

--- src/test/java/com/raidrin/sakanu/SakanuApplicationTests.java
+++ src/test/java/com/raidrin/sakanu/SakanuApplicationTests.java
@@ -2,8 +2,10 @@ package com.raidrin.sakanu;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SakanuApplicationTests {
 
     @Test
